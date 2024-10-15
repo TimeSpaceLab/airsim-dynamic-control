52c6d42c37ca5823db4e36b898ad70c8d3b90238
Twiorigin <96510667+Twiorigin@users.noreply.github.com>
2024-10-15 21:46:56 +0900

    Add files via upload
    data_collection_record API & settings.json

--- PythonClient/multirotor/data_collection_record.py
+++ PythonClient/multirotor/data_collection_record.py
@@ -13,7 +13,14 @@ client.confirmConnection()
 
 client.enableApiControl(True)
 client.armDisarm(True)
+
+
+
+
 client.takeoffAsync().join()
+
+
+
 
 client.startRecording()
 
@@ -21,14 +28,15 @@ client.moveToPositionAsync(0, 0, -450, 30).join()
 
 
 waypoints = [
-    (37.6682, 126.7242),  
-    (37.6634, 126.7213),  
-    (37.6177, 126.7970),  
-    (37.5959787, 126.8077136),  
-    (37.5724, 126.8045)
+    (37.5226, 126.9171),  
+    (37.5241, 126.9288),  
+    (37.5437627, 126.9046052)
+    # ,  
+    # (37.5959787, 126.8077136),  
+    # (37.5724, 126.8045)
 ]
 altitude = 450
-speed = 30
+speed = 5
 
 for waypoint in waypoints:
     latitude, longitude = waypoint
